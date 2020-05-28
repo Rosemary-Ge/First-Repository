37ba361641d3cc8dcc6bcce77de4c501f1b4c79a
Ge <gey13@asia.roche.com>
2020-05-28 17:30:21 +0800

    Capstone Project Notebook - assignment for week 1

---
+++ CapstoneProject.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h4>This notebook will be mainly used for the capstone project<h4>
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
